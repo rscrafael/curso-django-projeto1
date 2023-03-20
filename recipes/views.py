21458258faa63dd8abf8681f2719a882d4fa47c1
Rafael Soares Cardozo <rscrafael@gmail.com>
2023-03-20 18:09:37 -0300

    Removido arquivos

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,7 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html')
-
-
-def contato(request):
-    return HttpResponse('CONTATO')
-
-
-def sobre(request):
-    return HttpResponse('SOBRE')
+    return render(request, 'recipes/home.html', context={
+        'name': "Rafael S. Cardozo"
+    })
